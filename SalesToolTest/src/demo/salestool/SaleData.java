984df73a59095291fd6f303ea3c529f8d83aff79
Jyothi Kamisetty <Jyothi Kamisetty@Jyothi>
2015-04-08 13:11:53 -0400

    Completed Diplaying data in a sales data object

--- SalesToolTest/src/demo/salestool/SaleData.java
+++ SalesToolTest/src/demo/salestool/SaleData.java
@@ -5,8 +5,9 @@ public class SaleData {
 	int[] data = { 1, 2, 3 };
 
 	public void display() {
+		System.out.println("Data: ");
 		for (int i = 0; i < data.length; i++) {
-			System.out.println("TODO: Generated code goes here!!");
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 
