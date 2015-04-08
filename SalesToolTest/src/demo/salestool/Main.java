a7006256acb4af1d9180b16108898104898d96c5
Jyothi Kamisetty <Jyothi Kamisetty@Jyothi>
2015-04-08 12:08:47 -0400

    Just added a print statement

--- SalesToolTest/src/demo/salestool/Main.java
+++ SalesToolTest/src/demo/salestool/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE");
 		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
 	}
 
 	public static void main(String[] args) {
